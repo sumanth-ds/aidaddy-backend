15129874989484bb15f1354b9cf4016b0c4bba83
P SUMANTH REDDY <78702359+sumanth-ds@users.noreply.github.com>
2025-11-21 04:30:22 +0530

    port and host updated

--- backend/app.py
+++ backend/app.py
@@ -332,4 +332,5 @@ def delete_meeting(meeting_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    app.run(debug=True, host='0.0.0.0', port=5000)
